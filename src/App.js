2f65235f9910c530407bff283b784203ff94ff0c
elliot sherman <sheveryosef@gmail.com>
2022-01-10 00:42:03 +0200

    Added Styling
    added basic styling to the page played around with colors margins and more

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ import { Outlet } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <header className=''>this is a header</header>
+      <header className='d-flex justify-content-around bg-secondary bg-opacity-50 bg-gradient border-bottom border-secondary mb-1'><h1 className='text-white'>Elliot Sherman</h1> <h2 className='text-primary align-self-center fw-lighter'><u>Web Dev</u></h2></header>
       <div className="container-fluid">
         <main
-          className='d-flex row col-md border border-warning'
+          className='d-flex row mx-2'
         >
           <div className='row-2 col-md-2 col-lg-2 navbar-dark bg-secondary bg-gradient rounded-3'>
             <Navbar />
           </div>
           <div style={{ height: '800px' }}
-            className=' row-8 col-md-10 col-lg-8 border border-danger'>
+            className=' row-8 col-md-10 col-lg-8'>
             main article
             <Outlet />
           </div>
@@ -27,8 +27,7 @@ function App() {
         </main >
       </div>
 
-      <div className='border border-info position-relative bottom-0'>
-        footer
+      <div className='border border-info position-relative bottom-0 mt-1'>
         <Footer />
       </div>
     </div>
